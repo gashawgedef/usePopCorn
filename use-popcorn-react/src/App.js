b1c299f012c1a1a82716bc9011538548179decb1
gashawgedef@gmail.com <gashawgedef@gmail.com>
2023-10-04 11:47:41 -0700

    modularize the large functions

--- use-popcorn-react/src/App.js
+++ use-popcorn-react/src/App.js
@@ -114,9 +114,7 @@ function WatchedBox(){
   const [watched, setWatched] = useState(tempWatchedData);
   const [isOpen2, setIsOpen2] = useState(true);
 
-  const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
-  const avgUserRating = average(watched.map((movie) => movie.userRating));
-  const avgRuntime = average(watched.map((movie) => movie.runtime));
+
 
 
  return(
@@ -129,7 +127,21 @@ function WatchedBox(){
   </button>
   {isOpen2 && (
     <>
-      <div className="summary">
+      
+    < WatchedSummary watched={watched}/>
+     <WatchedList watched={watched}/>
+    </>
+  )}
+</div>
+ )
+}
+
+function WatchedSummary({watched}){
+  const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
+  const avgUserRating = average(watched.map((movie) => movie.userRating));
+  const avgRuntime = average(watched.map((movie) => movie.runtime));
+  return(
+    <div className="summary">
         <h2>Movies you watched</h2>
         <div>
           <p>
@@ -150,51 +162,64 @@ function WatchedBox(){
           </p>
         </div>
       </div>
+  )
+}
 
-      <ul className="list">
-        {watched.map((movie) => (
-          <li key={movie.imdbID}>
-            <img src={movie.Poster} alt={`${movie.Title} poster`} />
-            <h3>{movie.Title}</h3>
-            <div>
-              <p>
-                <span>⭐️</span>
-                <span>{movie.imdbRating}</span>
-              </p>
-              <p>
-                <span>🌟</span>
-                <span>{movie.userRating}</span>
-              </p>
-              <p>
-                <span>⏳</span>
-                <span>{movie.runtime} min</span>
-              </p>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </>
-  )}
-</div>
+function WatchedList({watched}){
+ return(
+  <ul className="list">
+  {watched.map((movie) => (
+    <WatchedMovie movie={movie} key={movie.imdbID}/>
+  ))}
+</ul>
  )
+}
+
+function WatchedMovie({movie}){
+  return(
+    <li >
+      <img src={movie.Poster} alt={`${movie.Title} poster`} />
+      <h3>{movie.Title}</h3>
+      <div>
+        <p>
+          <span>⭐️</span>
+          <span>{movie.imdbRating}</span>
+        </p>
+        <p>
+          <span>🌟</span>
+          <span>{movie.userRating}</span>
+        </p>
+        <p>
+          <span>⏳</span>
+          <span>{movie.runtime} min</span>
+        </p>
+      </div>
+    </li>
+  )
 }
 function MovieList(){
   const [movies, setMovies] = useState(tempMovieData);
   return(
     <ul className="list">
         {movies?.map((movie) => (
-          <li key={movie.imdbID}>
-            <img src={movie.Poster} alt={`${movie.Title} poster`} />
-            <h3>{movie.Title}</h3>
-            <div>
-              <p>
-                <span>🗓</span>
-                <span>{movie.Year}</span>
-              </p>
-            </div>
-          </li>
+         <Movie movie={movie} key={movie.imdbID} />
         ))}
       </ul>
+  )
+}
+
+function Movie({movie}){
+  return(
+    <li >
+    <img src={movie.Poster} alt={`${movie.Title} poster`} />
+    <h3>{movie.Title}</h3>
+    <div>
+      <p>
+        <span>🗓</span>
+        <span>{movie.Year}</span>
+      </p>
+    </div>
+  </li>
   )
 }
   function Main(){
