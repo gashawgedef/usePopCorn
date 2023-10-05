bdb64dc306401de2d5968f7a1ee2758d64dcc046
gashawgedef@gmail.com <gashawgedef@gmail.com>
2023-10-05 14:38:33 -0700

    hovering on stars

--- use-popcorn-react/src/StarRating.js
+++ use-popcorn-react/src/StarRating.js
@@ -26,11 +26,12 @@ export default function StarRating({ maxRating = 5 }) {
       <div style={starContainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
           <span>
-            <Star key={i} onRate={()=>handleRating(i+1)} full={rating>=i+1}/>
+            <Star key={i} onRate={()=>handleRating(i+1)} full={tempRating?tempRating>=i+1:rating>=i+1}onHoverIn={()=>setTempRating(i+1)}
+            onHoverOut={()=>setTempRating(0)}/>
           </span>
         ))}
       </div>
-      <p style={textStyle}>{rating||""}</p>
+      <p style={textStyle}>{tempRating||rating||""}</p>
     </div>
   );
 }
@@ -42,9 +43,9 @@ export default function StarRating({ maxRating = 5 }) {
  }
 
 
-function Star({onRate,full}) {
+function Star({onRate,full,onHoverIn,onHoverOut}) {
   return (
-    <span role="button" style={starStyle} onClick={onRate} >
+    <span role="button" style={starStyle} onClick={onRate}onMouseEnter={onHoverIn}  onMouseLeave={onHoverOut}>
      {full?( <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 20 20"
--- use-popcorn-react/src/index.js
+++ use-popcorn-react/src/index.js
@@ -8,7 +8,7 @@ import StarRating from './StarRating';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <StarRating maxRating={5}/>
+    <StarRating maxRating={10}/>
   </React.StrictMode>
 );
 
