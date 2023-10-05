132b8b1da3d5b02356f5a3c1fbbf4c571fceb525
gashawgedef@gmail.com <gashawgedef@gmail.com>
2023-10-05 15:00:21 -0700

    prop driling and component composition final

--- use-popcorn-react/src/index.js
+++ use-popcorn-react/src/index.js
@@ -8,7 +8,8 @@ import StarRating from './StarRating';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <StarRating maxRating={10}/>
+    <StarRating />
+    <StarRating size={24} color='red'/>
   </React.StrictMode>
 );
 
